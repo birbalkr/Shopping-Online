66bd0e9af50979d9316f5e92d0742ef3c90dcc6a
birbalkr <birbalkr1435@gmail.com>
2024-11-15 23:57:09 +0530

    Update Navbar

--- Shopping/src/App.jsx
+++ Shopping/src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
