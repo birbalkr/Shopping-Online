008e7e436e7842707146257c64fa20cf50b5bfdf
birbalkr <birbalkr1435@gmail.com>
2024-11-17 02:29:41 +0530

    Add All Product Entity

---
+++ shopping_Backend/src/main/java/com/example/shopping_Backenddemo/entity/ProductCarMotorbikeEntity.java
@@ -0,0 +1,24 @@
+package com.example.shopping_Backenddemo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name="carmotorbike")
+public class ProductCarMotorbikeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private String price;
+    private String image;
+    private String reviews;
+}
---
+++ shopping_Backend/src/main/java/com/example/shopping_Backenddemo/entity/ProductHomeKitchenEntity.java
@@ -0,0 +1,25 @@
+package com.example.shopping_Backenddemo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+
+
+@Data
+@Entity
+@Table(name="homekitchen")
+public class ProductHomeKitchenEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private String price;
+    private String image;
+    private String reviews;
+}
