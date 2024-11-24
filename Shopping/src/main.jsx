5b8b632112b578925b4106fda86006732716a298
prabhakar <prabhakarpandey7061@gmail.com>
2024-11-25 00:12:33 +0530

    solve the error

--- Shopping/src/main.jsx
+++ Shopping/src/main.jsx
@@ -4,12 +4,20 @@ import './index.css'
 import App from './App.jsx'
 import{createBrowserRouter, RouterProvider  } from 'react-router-dom'
 import Layout from './components/layout/Layout.jsx'
+import Home from './components/Home/Home.jsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout/>,
     children:[
+      {
+        path:"home",
+        element:<Home/>
+      },
+      {
+        
+      }
 
     ]
   }
