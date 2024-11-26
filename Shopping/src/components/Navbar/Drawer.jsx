7c0ba1a6f8f0318a21e8ac97d9efd8c341f17fc6
prabhakar <prabhakarpandey7061@gmail.com>
2024-11-26 11:15:52 +0530

    change Drawer menu  and set Media Screen

--- Shopping/src/components/Navbar/Drawer.jsx
+++ Shopping/src/components/Navbar/Drawer.jsx
@@ -4,129 +4,164 @@ import { Link } from "react-router-dom";
 export function Drawer() {
   return (
     <>
-     <div className="flex ml-12">
-     <div>
-        <a
-          className="btn mt-1"
-          data-bs-toggle="offcanvas"
-          href="#offcanvasExample"
-          role="button"
-          aria-controls="offcanvasExample"
-        >
-          <img src="/src/assets/images/menu.png" className="w-5" alt="" />
-        </a>
-        
-        <div
-          className=" bg-blue-gray-700 offcanvas offcanvas-start"
-          tabIndex="-1"
-          id="offcanvasExample"
-          aria-labelledby="offcanvasExampleLabel"
-        >
-          <div className="offcanvas-header">
-            <button
-              type="button"
-              className="btn-close"
-              data-bs-dismiss="offcanvas"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div className="offcanvas-body">
-            <div>
-              <img
-                src="/src/assets/images/logo.png"
-                className=" logo  w-18"
-                alt=""
-              />
+      <div className="flex ml-12">
+        <div>
+          <a
+            className="btn mt-1"
+            data-bs-toggle="offcanvas"
+            href="#offcanvasExample"
+            role="button"
+            aria-controls="offcanvasExample"
+          >
+            <img src="/src/assets/images/menu.png" className="w-5" alt="" />
+          </a>
+
+          <div
+            className="offcanvas offcanvas-start"
+            tabIndex="-1"
+            id="offcanvasExample"
+            aria-labelledby="offcanvasExampleLabel"
+          >
+            <div className="offcanvas-header">
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
             </div>
+            <div className="offcanvas-body">
+              <div>
+                <i class="fa-solid fa-circle-user"></i>
+                <Link to="">Hello, Sign in</Link>
+              </div>
+              <hr />
+              <h5 className="text-2xl ml-8 mt-3 font-bold">Shop by Category</h5>
+              <ul className="mt-3 ml-8">
+                <li className="my-2 mt-2 hover:bg-gray-100 text-xl cursor-pointer">
+                  <Link to="books">Books</Link>
+                </li>
 
-            <ul className="mt-14">
-              <li className="my-2 mt-2 hover:bg-gray-500 text-2xl cursor-pointer">
-                <Link className="flex flex-wrap p-6 hover:text-4xl" to="books">
-                  {" "}
-                  <img
-                    className="w-10 mr-7"
-                    src="/src/assets/images/book.png"
-                    alt=""
-                  />{" "}
-                  Books
-                </Link>
-              </li>
+                <li className="my-2 mt-2 hover:bg-gray-100  text-xl cursor-pointer ">
+                  <Link to="/computar">Computer</Link>
+                </li>
 
-              <li className="my-2 mt-2 hover:bg-gray-500  text-xl cursor-pointer ">
-                <Link
-                  className="flex flex-wrap p-6 hover:text-4xl"
-                  to="/computar"
-                >
-                  <img
-                    className="w-10 mr-7"
-                    src="/src/assets/images/computer.png"
-                    alt=""
-                  />
-                  Computer
-                </Link>
-              </li>
+                <li className="my-2 mt-2 hover:bg-gray-100 text-xl cursor-pointer">
+                  <Link to="/modile">Mobile</Link>
+                </li>
 
-              <li className="my-2 mt-2 hover:bg-gray-500 text-xl cursor-pointer">
-                <Link
-                  className="flex flex-wrap p-6 hover:text-4xl"
-                  to="/modile"
-                >
-                  <img
-                    className="w-10 mr-7"
-                    src="/src/assets/images/phone.png"
-                    alt=""
-                  />
-                  Mobile
-                </Link>
-              </li>
+                <li className="my-2 mt-2 hover:bg-gray-100 text-xl cursor-pointerr">
+                  <Link to="/homekitchan">Home&Kitchen</Link>
+                </li>
 
-              <li className="my-2 mt-2 hover:bg-gray-500 text-xl cursor-pointerr">
-                <Link
-                  className="flex flex-wrap p-6 hover:text-4xl"
-                  to="/homekitchan"
-                >
-                  <img
-                    className="w-10 mr-7"
-                    src="/src/assets/images/kitchen.png"
-                    alt=""
-                  />
-                  Home&Kitchen
-                </Link>
-              </li>
+                <li className="my-2 mt-2 hover:bg-gray-100  text-xl cursor-pointer">
+                  <Link to="/Sport">Sport</Link>
+                </li>
 
-              <li className="my-2 mt-2 hover:bg-gray-500  text-xl cursor-pointer">
-                <Link className="flex flex-wrap p-6 hover:text-4xl" to="/Sport">
-                  <img
-                    className="w-10 mr-7"
-                    src="/src/assets/images/sport.png"
-                    alt=""
-                  />
-                  Sport
-                </Link>
-              </li>
+                <li className="my-2 mt-2 hover:bg-gray-100  text-xl cursor-pointer">
+                  <Link to="/fashion">Fashion</Link>
+                </li>
+              </ul>
 
-              <li className="my-2 mt-2 hover:bg-gray-500  text-xl cursor-pointer">
-                <Link
-                  className="flex flex-wrap p-6 hover:text-4xl"
-                  to="/fashion"
-                >
-                  <img
-                    className="w-10 mr-7"
-                    src="/src/assets/images/fashion.png"
-                    alt=""
-                  />
-                  Fashion
-                </Link>
-              </li>
-            </ul>
+              <hr />
+              <h5 className="text-2xl ml-8 mt-3 font-bold">
+                Programs & Features
+              </h5>
+              <ul className="mt-3 ml-8">
+                <li className="my-2 mt-2 hover:bg-gray-100 text-xl cursor-pointer">
+                  <Link to="books">Online Pay</Link>
+                </li>
+
+                <li className="my-2 mt-2 hover:bg-gray-100  text-xl cursor-pointer ">
+                  <Link to="/computar">Gift Cards</Link>
+                </li>
+
+                <li className="my-2 mt-2 hover:bg-gray-100 text-xl cursor-pointer">
+                  <Link to="/modile">Mobile Reacharges</Link>
+                </li>
+
+                <li className="my-2 mt-2 hover:bg-gray-100 text-xl cursor-pointerr">
+                  <Link to="/homekitchan">Online Custom</Link>
+                </li>
+
+                <li className="my-2 mt-2 hover:bg-gray-100  text-xl cursor-pointer">
+                  <Link to="/Sport">Flight Tickets</Link>
+                </li>
+
+                <li className="my-2 mt-2 hover:bg-gray-100  text-xl cursor-pointer">
+                  <Link to="/fashion">Google Play Recharge </Link>
+                </li>
+              </ul>
+
+              <hr />
+              <h5 className="text-2xl ml-8 mt-3 font-bold">
+              Help & Settings
+              </h5>
+              <ul className="mt-3 ml-8">
+                <li className="my-2 mt-2 hover:bg-gray-100 text-xl cursor-pointer">
+                  <Link to="books">Your Account</Link>
+                </li>
+
+                <li className="my-2 mt-2 hover:bg-gray-100  text-xl cursor-pointer ">
+                  <Link to="/computar">Customer Service</Link>
+                </li>
+
+                <li className="my-2 mt-2 hover:bg-gray-100 text-xl cursor-pointer">
+                  <Link to="/modile">Sign in</Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
 
-      <marquee behavior="alternate">
-          This Is Shopping App Created by Birbal Kumar And Parbhakar Kumar
-        </marquee>
-     </div>
+        <div>
+          <ul className="flex flex-wrap mt-2.5 menuitem">
+            <li className="ml-5">
+              <Link to="books">Books</Link>
+            </li>
+
+            <li className="ml-5">
+              <Link to="/computar">Computer</Link>
+            </li>
+
+            <li className="ml-5">
+              <Link to="/modile">Mobile</Link>
+            </li>
+
+            <li className="ml-5 liItem ">
+              <Link to="/homekitchan">Home&Kitchen</Link>
+            </li>
+
+            <li className="ml-5 liItem ">
+              <Link to="/fashion">Fashion</Link>
+            </li>
+            <li className="ml-5 liItem ">
+              <Link to="/Sport">Beauty & Personal Care</Link>
+            </li>
+
+            <li className="ml-5 liItem">
+              <Link to="/fashion">Toys & Games Gift Cards</Link>
+            </li>
+            <li className="ml-5 liItem tabletitem laptop">
+              <Link to="/Sport">Grocery & Gourmet Foods Baby</Link>
+            </li>
+
+            <li className="ml-5 liItem tabletitem laptop">
+              <Link to="/fashion">Health, Household & Personal Care</Link>
+            </li>
+            <li className="ml-5 liItem tabletitem laptop">
+              <Link to="/Sport"> Sports</Link>
+            </li>
+
+            <li className="ml-5 liItem tabletitem laptop">
+              <Link to="/fashion">Fitness & Outdoors</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <marquee behavior="">
+        This Is Shopping App Created by Birbal Kumar And Parbhakar Kumar
+      </marquee>
     </>
   );
 }
